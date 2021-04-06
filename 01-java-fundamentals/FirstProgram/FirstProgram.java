1f249195c1fdfd4bd318f22829cd350f0beddd26
Adan Lara <adanlara8@gmail.com>
2021-04-05 21:56:16 -0700

    FirstProgram

---
+++ 01-java-fundamentals/FirstProgram/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Adan Lara");
+        System.out.println("I am 28 Years old");
+        System.out.println("My hometown is Long Beach, CA");
+
+    }
+}
